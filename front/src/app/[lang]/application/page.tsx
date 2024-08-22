2e7254881cd0a641b96f7a373762392b27f6dabe
Achraf <achrafelkhamsi@gmail.com>
2024-08-22 16:26:45 +0200

    add auth guard for application and team forms

--- front/src/app/[lang]/application/page.tsx
+++ front/src/app/[lang]/application/page.tsx
@@ -5,10 +5,15 @@ import { ApplicationForm } from "./form/application-form";
 import { useRecoilValue } from "recoil";
 import { userState } from "@/store/userState";
 import { Button } from "@/components/shared";
+import { useAuthGuard } from "@/components/hooks/use-auth-guard";
+import ProfileSkeleton from "../profile/profile-skeleton";
 
 
 export default function ApplicationPage() {
   const userData = useRecoilValue<any>(userState);
+  useAuthGuard();
+
+  if (!userData) return <ProfileSkeleton />;
 
   return (
     <div className="z-10 w-full px-5 max-w-screen-xl xl:px-0">
--- front/src/app/[lang]/team/page.tsx
+++ front/src/app/[lang]/team/page.tsx
@@ -3,10 +3,15 @@
 import { TeamForm } from "./form/team-form";
 import { useRecoilValue } from "recoil";
 import { userState } from "@/store/userState";
+import { useAuthGuard } from "@/components/hooks/use-auth-guard";
+import ProfileSkeleton from "../profile/profile-skeleton";
 
 
 export default function ApplicationPage() {
   const userData = useRecoilValue<any>(userState);
+  useAuthGuard();
+
+  if (!userData) return <ProfileSkeleton />;
 
   return (
     <div className="z-10 w-full px-5 max-w-screen-xl xl:px-0">
