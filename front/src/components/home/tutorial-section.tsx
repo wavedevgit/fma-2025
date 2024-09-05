06f89f08bbed4968970b356f9d3b7b5fb6539863
Achraf <achrafelkhamsi@gmail.com>
2024-09-05 09:48:43 +0200

    handle responsiveness of tutorial section in homepage

--- front/src/components/home/tutorial-section.tsx
+++ front/src/components/home/tutorial-section.tsx
@@ -7,10 +7,11 @@ const TutorialSection = () => {
         Nous vous avons préparé un tutoriel pour vous guider dans votre candidature et votre choix d&apos;équipe
       </div>
 
-      <div
-        className="drop-shadow-sm space-y-6 md:space-y-0 md:flex md:justify-around"
+      <div 
+        className="flex justify-around flex-wrap gap-6 p-8 rounded-lg animate-fade-up opacity-0"
+        style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
       >
-        <iframe width="710" height="444" src="https://www.youtube.com/embed/bLxunPmuipQ?si=h58D0wPOWl11apqj" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe width="718" height="450" src="https://www.youtube.com/embed/bLxunPmuipQ?si=h58D0wPOWl11apqj" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </div>
     </div>
   )
