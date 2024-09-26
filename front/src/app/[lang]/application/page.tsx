6c7a99af43f028dfa1963d87fcae5a3e52ec87e1
Achraf <achrafelkhamsi@gmail.com>
2024-09-27 01:00:36 +0200

    close applications

--- front/src/app/[lang]/application/page.tsx
+++ front/src/app/[lang]/application/page.tsx
@@ -5,18 +5,23 @@ import { useRecoilValue } from "recoil";
 import { userState } from "@/store/userState";
 import { useAuthGuard } from "@/components/hooks/use-auth-guard";
 import ProfileSkeleton from "../profile/profile-skeleton";
+import { useRouter } from "next/navigation";
 
 export default function ApplicationPage() {
   const userData = useRecoilValue<any>(userState);
+  const router = useRouter();
   useAuthGuard();
+  router.push('/profile/application')
 
-  if (!userData) return <ProfileSkeleton />;
+  // if (!userData) return <ProfileSkeleton />;
 
-  return (
-    <div className="z-10 w-full px-5 max-w-screen-xl xl:px-0">
-      <div className="space-y-6 p-10 pb-16">
-        <ApplicationForm userData={userData} />
-      </div>
-    </div>
-  )
+  // return (
+  //   <div className="z-10 w-full px-5 max-w-screen-xl xl:px-0">
+  //     <div className="space-y-6 p-10 pb-16">
+  //       <ApplicationForm userData={userData} />
+  //     </div>
+  //   </div>
+  // )
+
+  return <ProfileSkeleton />
 }
--- front/src/app/[lang]/team/page.tsx
+++ front/src/app/[lang]/team/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { TeamForm } from "./form/team-form";
 import { useRecoilValue } from "recoil";
 import { userState } from "@/store/userState";
 import { useAuthGuard } from "@/components/hooks/use-auth-guard";
@@ -12,18 +11,21 @@ export default function ApplicationPage() {
   const userData = useRecoilValue<any>(userState);
   const router = useRouter();
   useAuthGuard();
+  router.push('/profile/team');
 
-  if (!userData) {
-    return <ProfileSkeleton />;
-  } else if (!userData?.application || userData?.application?.status?.status === 'DRAFT' || userData?.team) {
-    router.push('/profile/team')
-  }  else {
-    return (
-      <div className="z-10 w-full px-5 max-w-screen-xl xl:px-0">
-        <div className="space-y-6 p-10 pb-16">
-          <TeamForm />
-        </div>
-      </div>
-    )
-  }
+  // if (!userData) {
+  //   return <ProfileSkeleton />;
+  // } else if (!userData?.application || userData?.application?.status?.status === 'DRAFT' || userData?.team) {
+  //   router.push('/profile/team')
+  // }  else {
+  //   return (
+  //     <div className="z-10 w-full px-5 max-w-screen-xl xl:px-0">
+  //       <div className="space-y-6 p-10 pb-16">
+  //         <TeamForm />
+  //       </div>
+  //     </div>
+  //   )
+  // }
+
+  return <ProfileSkeleton />
 }
